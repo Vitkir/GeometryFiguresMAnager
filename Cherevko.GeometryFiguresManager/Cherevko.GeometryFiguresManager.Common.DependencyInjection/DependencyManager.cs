c85dcaf6eaf49943c346919543386e45d925a01f
Vitkir <kirillcherevko@gmail.com>
2020-03-31 23:38:32 +0400

    dependency injection implemented

--- Cherevko.GeometryFiguresManager/Cherevko.GeometryFiguresManager.Common.DependencyInjection/DependencyManager.cs
+++ Cherevko.GeometryFiguresManager/Cherevko.GeometryFiguresManager.Common.DependencyInjection/DependencyManager.cs
@@ -1,6 +1,18 @@
-﻿namespace Cherevko.GeometryFiguresManager.Common.DependencyInjection
+﻿using Cherevko.GeometryFiguresManager.BLL.Contract;
+using Cherevko.GeometryFiguresManager.BLL.Kernel;
+using Cherevko.GeometryFiguresManager.DAL.Contract;
+using Cherevko.GeometryFiguresManager.DAL.MSSQL;
+using Ninject.Modules;
+
+namespace Cherevko.GeometryFiguresManager.Common.DependencyInjection
 {
-	public class DependencyManager
+	public class DependencyManager : NinjectModule
 	{
+		public override void Load()
+		{
+			Bind<IFigureDao>().To<FigureDao>();
+			Bind<ICache>().To<Cache>();
+			Bind<ILogic>().To<FigureLogic>();
+		}
 	}
 }
